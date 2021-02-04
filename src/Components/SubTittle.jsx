6aa8092597b32191f4b5aafbf40e677994249939
timera-h <timera.barry@gmail.com>
2021-02-04 12:40:30 +0100

    sous titre

---
+++ src/Components/SubTittle.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+  const SubTittle = styled.h2`
+    font-weight: 500;
+    font-size: 30px;
+    text-align: center;
+    color: white;
+    margin: 0 0 30px 0;
+  `;
+
+export default SubTittle;
