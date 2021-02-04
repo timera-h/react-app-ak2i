6291bb233ed7c3ff8f0617f0dce540b281c72bfe
timera-h <timera.barry@gmail.com>
2021-02-04 10:34:16 +0100

    project Card

--- src/Components/OurProjects.jsx
+++ src/Components/OurProjects.jsx
@@ -1,33 +1,71 @@
 import React from "react";
 import styled from "styled-components";
+import ProjectCard from "../utils/ProjectCard";
 
 export default function OurProjects() {
   const Container = styled.section`
     width: 100%;
-    min-height: 100vh;
+    min-height: 150vh;
     background: #2f2b53;
     color: white;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
     align-items: center;
   `;
 
   const SubTittle = styled.h2`
     font-weight: 500;
     font-size: 30px;
+    text-align: center;
+    margin: 30px
   `;
 
   const Paragraphe = styled.p`
     width: 25vw;
-    text-align: center
+    text-align: center;
+  `;
+
+  const ContainerProjectCard = styled.article`
+    display: flex;
+    flex-wrap: wrap;
+    width: 50%;
+    margin: 150px 0 0;
+    justify-content: center;
+    align-items: center;
+    -ms-transform: rotate(45deg); /* Internet Explorer */
+    -moz-transform: rotate(45deg); /* Firefox */
+    -webkit-transform: rotate(45deg); /* Safari et Chrome */
+    -o-transform: rotate(45deg); /* Opera */
+  `;
+
+  const BlocCard = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
   `;
 
   return (
     <Container>
-      <SubTittle>NOS PROJETS</SubTittle>
-      <Paragraphe>L’expérience technique et méthodologique de nos équipes permet une
-flexibilité accrue dans la gestion de projets.</Paragraphe>
+        <SubTittle>NOS PROJETS</SubTittle>
+        <Paragraphe>
+          L’expérience technique et méthodologique de nos équipes permet une
+          flexibilité accrue dans la gestion de projets.
+        </Paragraphe>
+      <ContainerProjectCard>
+        <BlocCard>
+          <ProjectCard />
+          <ProjectCard />
+        </BlocCard>
+        <BlocCard>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </BlocCard>
+        <BlocCard>
+          <ProjectCard />
+          <ProjectCard />
+        </BlocCard>
+      </ContainerProjectCard>
     </Container>
   );
 }
---
+++ src/utils/ProjectCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+
+export default function ProjectCard() {
+
+    const Card = styled.div`
+    height: 300px;
+    width: 300px;
+    background: red;
+    margin: 10px
+    `;
+
+    return (
+        <Card>
+            
+        </Card>
+    )
+}
