67976077acbe2dc13d01a761ca3ca78a52d8d267
timera-h <timera.barry@gmail.com>
2021-02-04 12:39:25 +0100

    project card

--- src/utils/ProjectCard.jsx
+++ src/utils/ProjectCard.jsx
@@ -3,16 +3,76 @@ import styled from "styled-components";
 
 export default function ProjectCard() {
 
+    const ProjectDetails = [
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        },
+        {
+            logo: "",
+            developpementMobile: false,
+            auditRepriseCode : false,
+            developpementWeb: false, 
+        }
+    ];
+
+    function AfficheFigcaption() {
+        for(let i = 0; i < ProjectCard.length; i++){
+            
+        }
+    }
+
     const Card = styled.div`
     height: 300px;
     width: 300px;
-    background: red;
-    margin: 10px
+    border: 1px solid rgb(225, 225, 225, .3);
+    margin: 10px;
+    display: flex; 
+    justify-content: center;
+    align-items: center
+    `;
+
+    const Figcaption = styled.figcaption`
+        background: rgb(0,0,0,.5);
+        width: 220px;
+        height: 220px;
     `;
 
     return (
         <Card>
-            
+            <Figcaption></Figcaption>
         </Card>
     )
 }
