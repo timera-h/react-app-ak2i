35c7b0158be28d7ec2f95705459d46e08386f008
timera-h <timera.barry@gmail.com>
2021-02-16 17:19:38 +0100

    ajout des dependances nav-scroll + css webpack

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import { history } from "../_helpers";
-import { HomePage } from "../HomePage";
 
 // layout
-import Header from "../Layout/index";
+import NavBar from "../Layout/NavBar";
+import Footer from "../Layout/Footer";
+import Presentation from "../Components/Presentation";
+import OurExpertise from "../Components/OurExpertises";
+import OurProjects from "../Components/OurProjects";
+import OurTeam from "../Components/OurTeam";
+import Contact from "../Components/Contact";
+
+// style
+import "../style/style.css";
 
 class App extends React.Component {
   render() {
     return (
       <Router history={history}>
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Redirect from="*" to="/" />
-          </Switch>
+        <NavBar />
+        <main className="mainHome">
+        <Presentation/>
+        <OurExpertise/>
+        <OurProjects/>
+        <OurTeam/>
+        <Contact/>
         </main>
+        <Footer/>
       </Router>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader'
-            },
-            {
-                test : /\.png$/,
-                exclude: /(node_modules)/,
-                loader : 'file-loader'
-              }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    }
-}
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".css", ".scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.png$/,
+        exclude: /(node_modules)/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: "http://localhost:4000",
+    }),
+  },
+};
